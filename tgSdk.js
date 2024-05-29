a24a4c1ddd7fbac0ee36f59556d434f0226848b2
Nikolay <fallout27121997@gmail.com>
2024-05-29 15:35:06 +0300

    fix
    fix

--- tgSdk.js
+++ tgSdk.js
@@ -1,6 +1,8 @@
-function getValues() {
+function sendValuesToUnity() {
     var name = "ТестТг";
     let number = 10;
     const someText = "bal bla bla";
-    return JSON.stringify({ name: name, number: number, someText: someText });
+
+    var data = JSON.stringify({ name: name, number: number, someText: someText });
+    UnitySendMessage("YourUnityObjectName", "SetValues", data);
 }

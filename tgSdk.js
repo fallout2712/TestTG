bf5c490a90b6e393b5293ee4048a96a57eb53ba9
Nikolay <fallout27121997@gmail.com>
2024-05-29 15:31:41 +0300

    fix
    fix

--- tgSdk.js
+++ tgSdk.js
@@ -1,7 +1,6 @@
-setTimeout(function() {
+function getValues() {
     var name = "ТестТг";
     let number = 10;
     const someText = "bal bla bla";
-
-    SendMessage("tgSDKobj", JSON.stringify({ name: name, number: number, someText: someText }));
-}, 5000); // 5000 миллисекунд = 5 секунд
+    return JSON.stringify({ name: name, number: number, someText: someText });
+}

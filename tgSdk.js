15b2a5ef20297147876a34f412095e0e5fe02b30
Nikolay <fallout27121997@gmail.com>
2024-05-29 15:10:04 +0300

    fix
    fix

--- tgSdk.js
+++ tgSdk.js
@@ -1,8 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Проверяем, что tgSDKobj определен и не равен null
+if (typeof tgSDKobj !== 'undefined' && tgSDKobj !== null) {
     var name = "ТестТг";
     let number = 10;
     const someText = "bal bla bla";
 
     // Отправляем значения в Unity
     tgSDKobj.SendMessage('SetValues', JSON.stringify({ name: name, number: number, someText: someText }));
-});
+} else {
+    console.error("tgSDKobj is not defined or null");
+}

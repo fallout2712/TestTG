d8fb01aa7934a1a636d002ea1140fff6e21b0243
Nikolay <fallout27121997@gmail.com>
2024-05-29 21:05:07 +0300

    fix
    fix

--- tgSdk.js
+++
@@ -1,7 +0,0 @@
-function callUnityMethod() {
-    // Get the instance of the Unity object
-    var unityInstance = unityInstance || UnityLoader.instantiate("unityContainer", "Build/Build.json", {onProgress: UnityProgress});
-    
-    // Call the Unity method
-    unityInstance.SendMessage('test', 'CallHello');
-}

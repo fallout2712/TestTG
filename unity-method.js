71c6e8bcba9ca81757e3cf8a5ce306361d385645
Nikolay Tretiak <128061636+fallout2712@users.noreply.github.com>
2024-05-30 10:24:40 +0300

    Update unity-method.js

--- unity-method.js
+++ unity-method.js
@@ -1,6 +1,8 @@
 // unity-method.js
 const tg = window.Telegram.WebApp;
 function sendMessageToUnity(unityInstance) {
+    tg.ready();
+    tg.expand();
     var idTg = tg.initDataUnsafe?.user?.id;
     var fnTg = tg.initDataUnsafe?.user?.first_name;
     var lnTg = tg.initDataUnsafe?.user?.last_name;

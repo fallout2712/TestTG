c36949d9d7458b41b33d20e7601cbd4e141cef29
Nikolay Tretiak <128061636+fallout2712@users.noreply.github.com>
2024-05-30 10:24:55 +0300

    Update unity-method.js

--- unity-method.js
+++ unity-method.js
@@ -6,7 +6,7 @@ function sendMessageToUnity(unityInstance) {
     var idTg = tg.initDataUnsafe?.user?.id;
     var fnTg = tg.initDataUnsafe?.user?.first_name;
     var lnTg = tg.initDataUnsafe?.user?.last_name;
-    unityInstance.SendMessage('Scripts', 'TestF', "Hello my little friend");
+    unityInstance.SendMessage('Scripts', 'TestF', "Hello my little friend v 2");
     unityInstance.SendMessage('Scripts', 'TestF', fnTg + " " + lnTg);
     unityInstance.SendMessage('Scripts', 'TestFInt', idTg);
   }

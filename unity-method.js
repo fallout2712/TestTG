4d264ee08924318b323e32debe8f15d9186e16b9
Nikolay <fallout27121997@gmail.com>
2024-05-31 11:20:40 +0300

    fix
    fix

--- unity-method.js
+++ unity-method.js
@@ -1,7 +1,7 @@
 // unity-method.js
 const tg = window.Telegram.WebApp;
 const botApi = "7203291206:AAEKqgqzVlLtMLtKr4sTA-rcp3vEoBAb-N0";
-const sourceChatId = "604531364"; // Замените на идентификатор группы, полученный выше
+const sourceChatId = "4241176386"; // Замените на идентификатор группы, полученный выше
 const messageId = "2"; // Замените на идентификатор сообщения, который нужно переслать
 
 function sendMessageToUnity(unityInstance) {

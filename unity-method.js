0396f697a1a6ccc76bbad91dd4a69f1b0b6251a7
Nikolay <fallout27121997@gmail.com>
2024-05-31 14:36:39 +0300

    fix
    fix

--- unity-method.js
+++ unity-method.js
@@ -1,5 +1,6 @@
 // unity-method.js
 const tg = window.Telegram.WebApp;
+const tgLink = "https://t.me/SwixyKeyDrop_bot";
 
 function sendMessageToUnity(unityInstance) {
   tg.ready();
@@ -16,9 +17,45 @@ function sendMessageToUnity(unityInstance) {
   unityInstance.SendMessage('TG_info', 'SetLastName', tgLn);
   unityInstance.SendMessage('TG_info', 'SetTgInit');
 
-  shareToTelegram();
+  shareToTelegramLink();
 }
-function shareToTelegram() {
-  var query = encodeURIComponent('Ваш текст для шаринга');
-  tg.switchInlineQuery(query);
+
+function shareToTelegramLink() {
+  copyToClipboard(tgLink);
+}
+
+function copyToClipboard(text) {
+  if (navigator.clipboard && window.isSecureContext) {
+    // navigator.clipboard API доступен
+    navigator.clipboard.writeText(text).then(function () {
+      console.log('Ссылка скопирована в буфер обмена');
+    }).catch(function (err) {
+      console.error('Не удалось скопировать текст: ', err);
+    });
+  } else {
+    // fallback для старых браузеров
+    let textArea = document.createElement("textarea");
+    textArea.value = text;
+    // избегаем прокрутки на iOS устройствах
+    textArea.style.position = "fixed";
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+    textArea.style.padding = 0;
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+    textArea.style.background = 'transparent';
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand('copy');
+      console.log('Ссылка скопирована в буфер обмена');
+    } catch (err) {
+      console.error('Не удалось скопировать текст: ', err);
+    }
+    document.body.removeChild(textArea);
+  }
 }

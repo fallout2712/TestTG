375c1946c07729385fec03ef4b46428e28c833a3
Nikolay <fallout27121997@gmail.com>
2024-05-31 15:54:05 +0300

    fix
    fix

--- unity-method.js
+++ unity-method.js
@@ -16,32 +16,4 @@ function sendMessageToUnity(unityInstance) {
   unityInstance.SendMessage('TG_info', 'SetFirstName', tgFn);
   unityInstance.SendMessage('TG_info', 'SetLastName', tgLn);
   unityInstance.SendMessage('TG_info', 'SetTgInit');
-
-  shareToTelegramLink();
-}
-
-function shareToTelegramLink() {
-  var container = document.createElement('div');
-  container.style.padding = '10px';
-
-  var promptText = document.createElement('div');
-  promptText.textContent = 'Твоя реферальная ссылка:';
-  container.appendChild(promptText);
-
-  var linkInput = document.createElement('input');
-  linkInput.setAttribute('value', tgLink);
-  linkInput.setAttribute('readonly', true);
-  linkInput.style.width = '100%';
-  container.appendChild(linkInput);
-
-  var copyButton = document.createElement('button');
-  copyButton.textContent = 'Скопировать';
-  copyButton.addEventListener('click', function () {
-    linkInput.select();
-    document.execCommand('copy');
-    alert('Ссылка скопирована!');
-  });
-  container.appendChild(copyButton);
-
-  document.body.appendChild(container);
 }

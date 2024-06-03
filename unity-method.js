f5e1bc3de99075b69c44f1dc4bb04dc2da0ad51d
Nikolay <fallout27121997@gmail.com>
2024-06-03 16:20:17 +0300

    fix
    fix

--- unity-method.js
+++ unity-method.js
@@ -17,6 +17,9 @@ function sendMessageToUnity(unityInstance) {
   unityInstance.SendMessage('TG_info', 'SetLastName', tgLn);
   unityInstance.SendMessage('TG_info', 'SetTgInit');
 
-    // Debug message
+  // Debug message
   console.log("Our app-kaaaa version is 0.2");
+}
+function yourJavaScriptFunction() {
+  console.log("JavaScript function called from Unity!");
 }

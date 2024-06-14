d9d71d28befb5be43067d8c91b88e7c91be3ceb1
Nikolay Tretiak <128061636+fallout2712@users.noreply.github.com>
2024-06-14 11:01:10 +0300

    Update unity-method.js

--- unity-method.js
+++ unity-method.js
@@ -13,39 +13,20 @@ function sendMessageToUnity(unityInstance) {
   var tgLn = tg.initDataUnsafe?.user?.last_name;
 
   const initData = tg.initDataUnsafe;
-  console.log("initData: ", initData); // Отладка данных инициализации
-  const startParam = initData.start_param || 0; // Устанавливаем start_param в 0, если он не найден
+  const startParam = parseInt(initData.start_param) || 0;
 
   console.log(`Параметр start_param: ${startParam}`);
+  
   unityInstance.SendMessage('TG_info', 'PassedValueUrl', startParam);
 
   unityInstance.SendMessage('TG_info', 'SetId', tgId);
   unityInstance.SendMessage('TG_info', 'SetFirstName', tgFn);
   unityInstance.SendMessage('TG_info', 'SetLastName', tgLn);
-  unityInstance.SendMessage('TG_info', 'SetTgInit'); // Передаем параметр в Unity
+  unityInstance.SendMessage('TG_info', 'SetTgInit');
 
   // Debug message
   console.log("Our app-kaaaa version is 0.8");
-  console.log("Received parameter:", startParam);  // Добавлено для отладки
-}
-function myFunctionOne() {
-  console.log("myFunction complete");
-  const feedback = 'This is a test message from web app';
-  const data = JSON.stringify({ feedback });
-  tg.sendData(data);
-  console.log("Data sent: ", data); // Логирование отправляемых данных
-}
-function myFunctionTwo() {
-  const query = 'Test'; // Ваш запрос
-  const choose_chat_types = ['users', 'groups', 'channels']; // Корректные типы чатов
-  tg.switchInlineQuery(query, choose_chat_types);
-  console.log("myFunctionTwo complete");
-}
-function myFunctionThree(parameter) {
-  console.log("myFunctionThree complete" + parameter);
-  const messageText = "https://t.me/SwixyKeyDrop_bot";
-  const telegramLink = `https://t.me/share/url?url=${encodeURIComponent(messageText)}&text=`;
-  tg.openTelegramLink(telegramLink);
+  console.log("Received parameter:", startParam);
 }
 function inviteFriend() {
   console.log("inviteFriend complete");
